cf1a25577685eea36c9c5b7765433084d6e82377
Rob Rua <robrua@alumni.cmu.edu>
2015-11-24 22:42:56 -0500

    Add gzip encoding and static data api

--- merakikernel/common.py
+++ merakikernel/common.py
@@ -1,12 +1,19 @@
 import urllib.error
 import bottle
 import ujson
+import zlib
 
 def riot_endpoint(function):
     def wrapped(*args, **kwargs):
         try:
             response = ujson.dumps(function(*args, **kwargs))
-            bottle.response.content_type = "application/json; charset=UTF-8"
+
+            if "gzip" in bottle.request.headers["Accept-Encoding"]:
+	            gzip_compressor = zlib.compressobj(9, zlib.DEFLATED, zlib.MAX_WBITS | 16)
+	            response = gzip_compressor.compress(bytes(response, "UTF-8")) + gzip_compressor.flush()
+	            bottle.response.headers["Content-Encoding"] = "gzip"
+
+            bottle.response.headers["Content-Type"] = "application/json; charset=UTF-8"
             return response
         except urllib.error.HTTPError as e:
             bottle.abort(e.code)
