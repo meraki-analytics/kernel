c53f2222eac0273a4ae56b94c3b41ec859cb7ebb
jason <jjmaldonis@gmail.com>
2016-01-24 20:08:49 -0600

    added modules and integrations for champion masteries

---
+++ merakikernel/modules/championmasteryapi.py
@@ -0,0 +1,28 @@
+import bottle
+
+import merakikernel.riotapi.championmasteryapi
+import merakikernel.common
+
+
+@bottle.route("/api/lol/championmastery/location/<platformId>/player/<summonerId>/champion/<championId>", method=["GET", "OPTIONS"])
+@merakikernel.common.riot_endpoint
+def champion_mastery(platformId, summonerId, championId):
+    return merakikernel.riotapi.championmasteryapi.champion_mastery(platformId, summonerId, championId, dict(bottle.request.query))
+
+
+@bottle.route("/api/lol/championmastery/location/<platformId>/player/<summonerId>/champions", method=["GET", "OPTIONS"])
+@merakikernel.common.riot_endpoint
+def champion_masteries(platformId, summonerId):
+    return merakikernel.riotapi.championmasteryapi.champion_masteries(platformId, summonerId, dict(bottle.request.query))
+
+
+@bottle.route("/api/lol/championmastery/location/<platformId>/player/<summonerId>/score", method=["GET", "OPTIONS"])
+@merakikernel.common.riot_endpoint
+def champion_mastery_score(platformId, summonerId):
+    return merakikernel.riotapi.championmasteryapi.champion_mastery_score(platformId, summonerId, dict(bottle.request.query))
+
+
+@bottle.route("/api/lol/championmastery/location/{platformId}/player/{summonerId}/topchampions", method=["GET", "OPTIONS"])
+@merakikernel.common.riot_endpoint
+def top_champion_masteries(platformId, summonerId):
+    return merakikernel.riotapi.championmasteryapi.top_champion_masteries(platformId, summonerId, dict(bottle.request.query))
