1bd4a16feba7426a2b1ea78b610f856d4762cb6a
Rob Rua <robertrua@gmail.com>
2017-11-23 13:57:48 -0500

    move default pipeline subconfigs

--- kernel/src/main/java/com/merakianalytics/kernel/KernelConfiguration.java
+++ kernel/src/main/java/com/merakianalytics/kernel/KernelConfiguration.java
@@ -4,14 +4,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.deltaspike.core.api.exclude.Exclude;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.merakianalytics.datapipelines.PipelineElement;
 import com.merakianalytics.kernel.filters.CORSFilter;
 import com.merakianalytics.orianna.datapipeline.PipelineConfiguration;
 import com.merakianalytics.orianna.datapipeline.PipelineConfiguration.PipelineElementConfiguration;
@@ -21,8 +16,6 @@ import com.merakianalytics.orianna.types.common.Platform;
 
 @Exclude
 public class KernelConfiguration {
-    private static Logger LOGGER = LoggerFactory.getLogger(KernelConfiguration.class);
-
     private static PipelineConfiguration defaultPipelineConfiguration() {
         final PipelineConfiguration config = new PipelineConfiguration();
 
@@ -30,29 +23,10 @@ public class KernelConfiguration {
         config.setTransformers(transformers);
 
         final List<PipelineElementConfiguration> elements = ImmutableList.of(
-            element(RiotAPI.class));
+            PipelineElementConfiguration.defaultConfiguration(RiotAPI.class));
         config.setElements(elements);
 
         return config;
-    }
-
-    private static PipelineElementConfiguration element(final Class<? extends PipelineElement> clazz) {
-        final PipelineElementConfiguration element = new PipelineElementConfiguration();
-        element.setClassName(clazz.getCanonicalName());
-        for(final Class<?> subClazz : clazz.getDeclaredClasses()) {
-            // We're assuming there's a public static inner class named Configuration if the element needs a configuration.
-            if(subClazz.getName().endsWith("Configuration")) {
-                element.setConfigClassName(subClazz.getName());
-
-                try {
-                    final Object defaultConfig = subClazz.newInstance();
-                    element.setConfig(new ObjectMapper().setSerializationInclusion(Include.NON_DEFAULT).valueToTree(defaultConfig));
-                } catch(InstantiationException | IllegalAccessException e) {
-                    LOGGER.error("Failed to generate default configuration for " + clazz.getCanonicalName() + "!", e);
-                }
-            }
-        }
-        return element;
     }
 
     private CORSFilter.Configuration CORS = new CORSFilter.Configuration();
